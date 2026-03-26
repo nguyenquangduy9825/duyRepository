4cb0a0fdfa5bcc2c0ea1c316e6c03e273bf825f0
nguyenquangduy9825 <20233525@eaut.edu.vn>
2026-03-26 16:15:53 +0700

    Loại bỏ toàn bộ file khỏi sự quản lý của Git

--- bt.js
+++
@@ -1,8 +0,0 @@
-var year=parseInt(prompt("nhập năm vào đây "));
-if(year%4==0){
-    console.log("đây ko phải là năm nhuận ");
-}else if(year%100==0){
-    console.log("đây ko phải năm nhuận ");
-}else{
-    console.log("đây là năm nhuận ")
-}
